a40a17d0bd77e0825e2f0933232d153312cd1256
VB822 <vbatres@greenwichacademy.org>
2026-01-07 15:23:52 -0500

    initial code of students.java

---
+++ Students.java
@@ -0,0 +1,15 @@
+package MacEachern_Batres_Llamas_Repo;
+public class Students {
+    private String name;
+    private int grade;
+    public Students(String name, int grade) {
+    this.name = name;
+    this.grade = grade;
+    }
+    public String getName() {
+    return name;
+    }
+    public int getGrade() {
+    return grade;
+    }
+}
