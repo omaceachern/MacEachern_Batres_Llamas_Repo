c0192428eb552c1b0f084b843de016d50acc1923
VB822 <vbatres@greenwichacademy.org>
2026-01-09 12:31:51 -0500

    updated students for instructions

--- MainTester.java
+++ MainTester.java
@@ -2,6 +2,7 @@ package MacEachern_Batres_Llamas_Repo;
 public class MainTester {
     /*asks the user their name and grade
         constructs a student
+        also create a spoon object to use
         also automatically gives them their status
         if they are eliminated
             they cant do anything
