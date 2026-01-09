1e0fe2f5d716d9d9d682f4b7fcfe586b765ac1b8
VB822 <vbatres@greenwichacademy.org>
2026-01-09 12:12:52 -0500

    updating students.java

--- Spoons.java
+++ Spoons.java
@@ -1,4 +1,14 @@
 package MacEachern_Batres_Llamas_Repo;
 public class Spoons {
-    
+    /*Contains the data from the file and methods to access it
+    make an array list of students in 9,10,11,12, and faculty
+    assigns students by grade or if they are faculty
+    maps: keys: everyone playing, value: thier target (next on list)
+        find as key on map. value will be the eliminated players value
+        ex:
+        Batres: itzel
+        itzel: olivia
+        olivia: bono
+        bono: batres
+     */
 }
