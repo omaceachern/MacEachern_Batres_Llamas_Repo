55b340390ac329eef5e3a56e0454b20c0c31eae2
VB822 <vbatres@greenwichacademy.org>
2026-01-09 12:20:42 -0500

    new main tester and spoons

--- MainTester.java
+++ MainTester.java
@@ -1,4 +1,16 @@
 package MacEachern_Batres_Llamas_Repo;
 public class MainTester {
+    /*asks the user their name and grade
+        constructs a student
+        also automatically gives them their status
+        if they are eliminated
+            they cant do anything
+        if they are not eliminated
+            allow them to self report themselves as eliminated
+            or report someone else
+                eliminated
+                    go into spoon and change map
+                eliminated someone*
+                    go into spoon and change map*/
     
 }
